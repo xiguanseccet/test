c2c14ac22398d513d3d307cf7caa4f71398cd358
suibian <123123@qq.com>
2019-06-22 21:23:42 +0800

    idea第一次提交Git

---
+++ test01/src/com/it/tesst01.java
@@ -0,0 +1,15 @@
+package com.it;
+
+/**
+ * @author: 好好学习 天天向上!!
+ * @date : Created in 2019/6/22
+ * @description :
+ * @version: 1.0
+ */
+
+
+public class tesst01 {
+    public static void main(String[] args) {
+        System.out.println("hello,world!");
+    }
+}
